5f9781b8539c2a8fd87226a2ccb9e44481bf7c99
Allan <allan.deoliveira@outlook.com>
2023-11-21 17:12:13 +0100

    refactor : adding component footer.tsx

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -10,9 +10,10 @@ import Home from "./pages/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Footer from "./components/Footer/Footer"; // Assurez-vous que le chemin est correct
 
 /* Styles */
-import "./styles/scss/app.scss"
+import "./styles/scss/app.scss";
 
 /* Routes */
 const router = createBrowserRouter([
@@ -38,6 +39,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <RouterProvider router={router} />
+    <Footer theme="white" />{" "}
+    {/* Ajoutez le Footer ici, avec le prop 'theme' selon vos besoins */}
   </React.StrictMode>
 );
