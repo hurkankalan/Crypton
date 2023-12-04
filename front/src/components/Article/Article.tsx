546e3d0f3a4d8917ed4c855bce94f152ff66bc03
Allan <allan.deoliveira@outlook.com>
2023-12-04 15:45:46 +0100

    adding : pagination and article display

--- front/src/components/Article/Article.tsx
+++ front/src/components/Article/Article.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Article.scss";
-// import ParticleTwo from "./ParticleTwo";
+import articlesData from "./articles.json"; // Assurez-vous que le chemin est correct
+
+const ArticlesPerPage = 9; // Définir le nombre d'articles par page
 
 const Article: React.FC = () => {
+  // État pour suivre la page actuelle
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Calcul de l'index du dernier article sur la page actuelle
+  const indexOfLastArticle = currentPage * ArticlesPerPage;
+
+  // Calcul de l'index du premier article sur la page actuelle
+  const indexOfFirstArticle = indexOfLastArticle - ArticlesPerPage;
+
+  // Sélection des articles à afficher sur la page actuelle
+  const currentArticles = articlesData.slice(
+    indexOfFirstArticle,
+    indexOfLastArticle
+  );
+
+  // Fonction pour changer la page courante
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
   return (
     <>
       <div className="ht__bradcaump__area">
@@ -13,15 +33,6 @@ const Article: React.FC = () => {
               <div className="col-lg-12">
                 <div className="bradcaump__inner text-center">
                   <h2 className="bradcaump-title">Articles</h2>
-                  {/* <nav className="bradcaump-inner">
-                    <Link className="breadcrumb-item" to={"#"}>
-                      Home
-                    </Link>
-                    <span className="brd-separetor">/</span>
-                    <span className="breadcrumb-item active">
-                      {"Lorem Ipsum"}
-                    </span>
-                  </nav> */}
                 </div>
               </div>
             </div>
@@ -31,304 +42,51 @@ const Article: React.FC = () => {
       <div className="dg__blog__area bg--white pt--110 pb--140">
         <div className="container">
           <div className="row">
-            {/* Start Single Blog */}
-            <div className="col-lg-4 col-md-6 col-sm-6 col-12">
-              <article className="blog__3">
-                <div className="thumb">
-                  <Link to={"#"}>
-                    <img src={"#"} alt="" />
-                  </Link>
-                </div>
-                <div className="content">
-                  <div className="bl__author">
-                    <img src="./img/1.png" alt="author images" />
-                    <div className="author__inner">
-                      <h6>Farhan</h6>
-                      <span>16 Feb, 2020</span>
-                    </div>
-                  </div>
-                  <h2>
-                    <Link to={"#"}>
-                      Large Mining Farm Discovered in Abandoned Russian Factory
+            {currentArticles.map((article) => (
+              // Génération de chaque article
+              <div
+                key={article.id}
+                className="col-lg-4 col-md-6 col-sm-6 col-12"
+              >
+                <article className="blog__3">
+                  <div className="thumb">
+                    <Link to={article.pageUrl}>
+                      <img src={article.imageUrl} alt="author images" />
                     </Link>
-                  </h2>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text{" "}
-                  </p>
-                </div>
-              </article>
-            </div>
-            {/* End Single Blog */}
-            {/* Start Single Blog */}
-            <div className="col-lg-4 col-md-6 col-sm-6 col-12">
-              <article className="blog__3">
-                <div className="thumb">
-                  <Link to={"#"}>
-                    <img src={"#"} alt="" />
-                  </Link>
-                </div>
-                <div className="content">
-                  <div className="bl__author">
-                    <img src={"#"} alt="author images" />
-                    <div className="author__inner">
-                      <h6>Alex</h6>
-                      <span>16 Feb, 2020</span>
-                    </div>
                   </div>
-                  <h2>
-                    <Link to={"#"}>
-                      Phasellus semper felisnon imperdiet varius.
-                    </Link>
-                  </h2>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text{" "}
-                  </p>
-                </div>
-              </article>
-            </div>
-            {/* End Single Blog */}
-            {/* Start Single Blog */}
-            <div className="col-lg-4 col-md-6 col-sm-6 col-12">
-              <article className="blog__3">
-                <div className="thumb">
-                  <Link to={"#"}>
-                    <img src={"#"} alt="" />
-                  </Link>
-                </div>
-                <div className="content">
-                  <div className="bl__author">
-                    <img src={"#"} alt="author images" />
-                    <div className="author__inner">
-                      <h6>Maria</h6>
-                      <span>16 Feb, 2020</span>
+                  <div className="content">
+                    <div className="bl__author">
+                      <div className="author__inner">
+                        <h6>{article.source}</h6>
+                        <span>{article.date}</span>
+                      </div>
                     </div>
+                    <h2>
+                      <Link to={article.pageUrl}>{article.title}</Link>
+                    </h2>
+                    <p>{article.summary}</p>
                   </div>
-                  <h2>
-                    <Link to={"#"}>
-                      World’s Central Bank: Crypto Could Risk Bank Runs
-                    </Link>
-                  </h2>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text{" "}
-                  </p>
-                </div>
-              </article>
-            </div>
-            {/* End Single Blog */}
-            {/* Start Single Blog */}
-            <div className="col-lg-4 col-md-6 col-sm-6 col-12">
-              <article className="blog__3">
-                <div className="thumb">
-                  <Link to={"#"}>
-                    <img src={"#"} alt="" />
-                  </Link>
-                </div>
-                <div className="content">
-                  <div className="bl__author">
-                    <img src={"#"} alt="author images" />
-                    <div className="author__inner">
-                      <h6>Farhan</h6>
-                      <span>16 Feb, 2020</span>
-                    </div>
-                  </div>
-                  <h2>
-                    <Link to={"#"}>
-                      Blogger ‘Bitfinex’ed’ Hires Legal Counsel to Fight
-                      Bitfinex Lawsuit
-                    </Link>
-                  </h2>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text{" "}
-                  </p>
-                </div>
-              </article>
-            </div>
-            {/* End Single Blog */}
-            {/* Start Single Blog */}
-            <div className="col-lg-4 col-md-6 col-sm-6 col-12">
-              <article className="blog__3">
-                <div className="thumb">
-                  <Link to={""}>
-                    <img src={""} alt="" />
-                  </Link>
-                </div>
-                <div className="content">
-                  <div className="bl__author">
-                    <img src={""} alt="author images" />
-                    <div className="author__inner">
-                      <h6>James J Trump</h6>
-                      <span>16 Feb, 2020</span>
-                    </div>
-                  </div>
-                  <h2>
-                    <Link to={"#"}>
-                      World’s Central Bank: Crypto Could Risk Bank Runs
-                    </Link>
-                  </h2>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text{" "}
-                  </p>
-                </div>
-              </article>
-            </div>
-            {/* End Single Blog */}
-            {/* Start Single Blog */}
-            <div className="col-lg-4 col-md-6 col-sm-6 col-12">
-              <article className="blog__3">
-                <div className="thumb">
-                  <Link to={"#"}>
-                    <img src={"#"} alt="" />
-                  </Link>
-                </div>
-                <div className="content">
-                  <div className="bl__author">
-                    <img src={"#"} alt="author images" />
-                    <div className="author__inner">
-                      <h6>James J Trump</h6>
-                      <span>16 Feb, 2020</span>
-                    </div>
-                  </div>
-                  <h2>
-                    <Link to={"#"}>
-                      Phasellus semper felisnon imperdiet varius.
-                    </Link>
-                  </h2>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text{" "}
-                  </p>
-                </div>
-              </article>
-            </div>
-            {/* End Single Blog */}
-            {/* Start Single Blog */}
-            <div className="col-lg-4 col-md-6 col-sm-6 col-12">
-              <article className="blog__3">
-                <div className="thumb">
-                  <Link to={"#"}>
-                    <img src={"#"} alt="" />
-                  </Link>
-                </div>
-                <div className="content">
-                  <div className="bl__author">
-                    <img src={"#"} alt="author images" />
-                    <div className="author__inner">
-                      <h6>John Smith</h6>
-                      <span>16 Feb, 2020</span>
-                    </div>
-                  </div>
-                  <h2>
-                    <Link to={"#"}>
-                      World’s Central Bank: Crypto Could Risk Bank Runs
-                    </Link>
-                  </h2>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text{" "}
-                  </p>
-                </div>
-              </article>
-            </div>
-            {/* End Single Blog */}
-            {/* Start Single Blog */}
-            <div className="col-lg-4 col-md-6 col-sm-6 col-12">
-              <article className="blog__3">
-                <div className="thumb">
-                  <Link to={"#"}>
-                    <img src={"#"} alt="" />
-                  </Link>
-                </div>
-                <div className="content">
-                  <div className="bl__author">
-                    <img src={"#"} alt="author images" />
-                    <div className="author__inner">
-                      <h6>Robert</h6>
-                      <span>16 Feb, 2020</span>
-                    </div>
-                  </div>
-                  <h2>
-                    <Link to={"#"}>
-                      Phasellus semper felisnon imperdiet varius.
-                    </Link>
-                  </h2>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text{" "}
-                  </p>
-                </div>
-              </article>
-            </div>
-            {/* End Single Blog */}
-            {/* Start Single Blog */}
-            <div className="col-lg-4 col-md-6 col-sm-6 col-12">
-              <article className="blog__3">
-                <div className="thumb">
-                  <Link to={"#"}>
-                    <img src={"#"} alt="" />
-                  </Link>
-                </div>
-                <div className="content">
-                  <div className="bl__author">
-                    <img src={"#"} alt="author images" />
-                    <div className="author__inner">
-                      <h6>Robert Downey</h6>
-                      <span>16 Feb, 2020</span>
-                    </div>
-                  </div>
-                  <h2>
-                    <Link to={"#"}>
-                      Phasellus semper felisnon imperdiet varius.
-                    </Link>
-                  </h2>
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text{" "}
-                  </p>
-                </div>
-              </article>
-            </div>
-            {/* End Single Blog */}
+                </article>
+              </div>
+            ))}
           </div>
           <div className="row mt--40">
             <div className="col-lg-12">
+              {/* Pagination */}
               <ul className="dg__pagination d-flex">
-                <li>
-                  <a href={"/"}>01</a>
-                </li>
-                <li>
-                  <a href={"/"}>02</a>
-                </li>
-                <li>
-                  <a className="active" href={"/"}>
-                    03
-                  </a>
-                </li>
-                <li>
-                  <a href={"/"}>04</a>
-                </li>
-                <li>
-                  <a href={"/"}>05</a>
-                </li>
-                <li>
-                  <a href={"/"}>......</a>
-                </li>
-                <li>
-                  <a href={"/"}>10</a>
-                </li>
+                {/* Génération des numéros de page pour la pagination */}
+
+                {[
+                  ...Array(
+                    Math.ceil(articlesData.length / ArticlesPerPage)
+                  ).keys(),
+                ].map((number) => (
+                  <li key={number + 1}>
+                    <a onClick={() => paginate(number + 1)} href="#!">
+                      {number + 1}
+                    </a>
+                  </li>
+                ))}
               </ul>
             </div>
           </div>
