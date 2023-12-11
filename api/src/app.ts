b0934dbc4f4a98d8cd332e7360d724ad55c6fbe9
Daniel Viter <daniel.viter@epitech.eu>
2023-12-11 17:12:13 +0100

    Adding login to handle requests to back end

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,11 +2,14 @@ import express from "express";
 import { Request, Response } from "express";
 import cookie from "cookie-parser";
 import userRouter from "./routes/index.route";
+import cors from 'cors';
 import { getPressReview } from "./utils/rssArticles";
 
 const app = express();
 const port = 8000;
 
+// app.use(cors());
+app.use(cors({ credentials: true, origin: 'http://localhost:3001' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookie());
