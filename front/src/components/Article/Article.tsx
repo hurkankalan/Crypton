86499406b93938cac7a20e841f5a3acc5b5af411
Allan <allan.deoliveira@outlook.com>
2023-12-11 16:48:38 +0100

    stanged change from main

--- front/src/components/Article/Article.tsx
+++ front/src/components/Article/Article.tsx
@@ -1,34 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Article.scss";
-import articlesData from "./articles.json"; // Assurez-vous que le chemin est correct
 
-const ArticlesPerPage = 9; // Définir le nombre d'articles par page
+// Nombre d'articles à afficher par page
+const ArticlesPerPage = 9;
 
 const Article: React.FC = () => {
-  // État pour suivre la page actuelle
+  // 'articles' stocke la liste des articles récupérés de l'API
+  const [articles, setArticles] = useState([]);
+  // 'currentPage' garde la trace de la page actuelle pour la pagination
   const [currentPage, setCurrentPage] = useState(1);
 
-  // Calcul de l'index du dernier article sur la page actuelle
+  // useEffect est utilisé pour exécuter le code lors du montage du composant
+  useEffect(() => {
+    // Définition d'une fonction asynchrone pour récupérer les articles
+    const fetchArticles = async () => {
+      try {
+        // Appel de l'API pour récupérer les articles
+        const response = await fetch("http://localhost:8000/articles/search");
+        const data = await response.json(); // Transformation de la réponse en JSON
+        setArticles(data); // Mise à jour de l'état 'articles' avec les données récupérées
+        console.log(data); // Affiche les données dans la console
+      } catch (error) {
+        // Gestion des erreurs en cas d'échec de l'appel API
+        console.error("Erreur lors de la récupération des articles:", error);
+      }
+    };
+
+    fetchArticles(); // Appel de la fonction pour récupérer les articles
+  }, []);
+
+  const extractImageUrl = (htmlContent: string) => {
+    const regex = /<img.*?src=["'](.*?)["']/; // Regex pour trouver la balise img et capturer l'URL
+    const match = htmlContent.match(regex);
+    return match ? match[1] : null; // Retourne l'URL de l'image ou null si aucune image n'est trouvée
+  };
+
+  const removeImageFromSummary = (htmlContent: string) => {
+    const regex = /<img.*?src=["'].*?["'].*?>/; // Regex pour trouver et retirer la balise img
+    return htmlContent.replace(regex, ""); // Remplace la balise img par une chaîne vide
+  };
+
+  // Calcul de l'index du dernier et du premier article pour la pagination
   const indexOfLastArticle = currentPage * ArticlesPerPage;
-
-  // Calcul de l'index du premier article sur la page actuelle
   const indexOfFirstArticle = indexOfLastArticle - ArticlesPerPage;
-
-  // Sélection des articles à afficher sur la page actuelle
-  const currentArticles = articlesData.slice(
+  // Obtention des articles actuels à afficher sur la page
+  const currentArticles = articles.slice(
     indexOfFirstArticle,
     indexOfLastArticle
   );
 
-  // Fonction pour changer la page courante et remonter au début de la page
-  const paginate = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
-    window.scrollTo({
-      top: 0, // Remonte tout en haut de la page
-      behavior: "smooth", // Animation douce
-    });
-  };
+  // Fonction pour changer de page
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
   return (
     <>
@@ -48,8 +71,7 @@ const Article: React.FC = () => {
       <div className="dg__blog__area bg--white pt--110 pb--140">
         <div className="container">
           <div className="row">
-            {currentArticles.map((article) => (
-              // Génération de chaque article
+            {currentArticles.map((article: any) => (
               <div
                 key={article.id}
                 className="col-lg-4 col-md-6 col-sm-6 col-12"
@@ -57,20 +79,32 @@ const Article: React.FC = () => {
                 <article className="blog__3">
                   <div className="thumb">
                     <Link to={article.pageUrl}>
-                      <img src={article.imageUrl} alt="author images" />
+                      <img src={extractImageUrl(article.summary)} alt="" />
                     </Link>
                   </div>
                   <div className="content">
                     <div className="bl__author">
                       <div className="author__inner">
                         <h6>{article.source}</h6>
-                        <span>{article.date}</span>
+                        <span>
+                          {new Date(article.date).toLocaleDateString()}
+                        </span>
                       </div>
                     </div>
                     <h2>
-                      <Link to={article.pageUrl}>{article.title}</Link>
+                      <a
+                        href={article.pageUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {article.title}
+                      </a>
                     </h2>
-                    <p>{article.summary}</p>
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: removeImageFromSummary(article.summary),
+                      }}
+                    />
                   </div>
                 </article>
               </div>
@@ -81,9 +115,7 @@ const Article: React.FC = () => {
               {/* Pagination */}
               <ul className="dg__pagination d-flex">
                 {[
-                  ...Array(
-                    Math.ceil(articlesData.length / ArticlesPerPage)
-                  ).keys(),
+                  ...Array(Math.ceil(articles.length / ArticlesPerPage)).keys(),
                 ].map((number) => (
                   <li key={number + 1}>
                     <a
