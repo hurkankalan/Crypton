8ab6bc53faf805a446825ee8fac4c857a8d3c033
Mohammed Hikel <Mohammed-larbi.hikel@epitech.eu>
2023-12-12 16:56:00 +0100

    Refactor et rajouter les notfification

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -8,11 +8,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 /* Components */
 import Home from "./pages/Home";
 import MarketTable from "./components/market-tables/MarketTable.tsx";
+import { ToastContainer } from 'react-toastify';
 
 /* Styles */
 import "./styles/scss/app.scss";
 import "./styles/scss/_global.scss";
-
+import 'react-toastify/dist/ReactToastify.css';
 import Container from "./pages/Container/Container.tsx";
 import Article from "./components/Article/Article.tsx";
 import Login from "./components/Login/Login.tsx"
@@ -49,5 +50,6 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <ToastContainer />
   </React.StrictMode>
 );
--- front/src/pages/Container/Container.tsx
+++ front/src/pages/Container/Container.tsx
@@ -2,14 +2,9 @@ import { useLocation, useOutlet } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 import { Navbar } from '../../components/Navbar/Navbar';
 import styles from './Container.module.scss';
-import { useEffect } from 'react';
 
 export default function Container() {
   const location = useLocation();
-
-  useEffect(() => {
-    console.log(location);
-  }, [location]);
 
   return (
     <>
