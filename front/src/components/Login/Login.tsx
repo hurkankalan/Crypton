7d3cf7c1def7ef3467dddcc5a18d86de31df161e
Daniel Viter <daniel.viter@epitech.eu>
2023-12-19 15:43:15 +0100

    Adding feature to log with discord

--- front/src/components/Login/Login.tsx
+++ front/src/components/Login/Login.tsx
@@ -1,18 +1,21 @@
 import Tab from "react-bootstrap/Tab";
 import Nav from "react-bootstrap/Nav";
 import "./Login.Module.scss"
-import { login, register, /*logout*/ } from '../api/auth.api.tsx';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDiscord } from '@fortawesome/free-brands-svg-icons';
-import { Link } from "react-router-dom";
-import { ChangeEvent, useState } from 'react';
+import {login, loginDiscord, register} from '../api/auth.api.tsx';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faDiscord} from '@fortawesome/free-brands-svg-icons';
+import {ChangeEvent, useState} from 'react';
+
+
 const LoginRegister = () => {
+    const [activeTab, setActiveTab] = useState('login');
     const [emailInput, setEmailInput] = useState('');
     const [passwordInput, setPasswordInput] = useState('');
     const [usernameCreate, setUsernameCreate] = useState('');
     const [passwordCreate, setPasswordCreate] = useState('');
     const [emailCreate, setEmailCreate] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+
 
     const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
         setEmailInput(event.target.value);
@@ -31,17 +34,66 @@ const LoginRegister = () => {
         setPasswordCreate(event.target.value);
     };
 
-    const handleDiscordAuthClick = () => {
-        const clientId = 'YOUR_DISCORD_CLIENT_ID';
-        const redirectUri = 'YOUR_REDIRECT_URI'; // This should be set up in your Discord Developer Portal
+    interface Params {
+        [key: string]: string;
+    }
 
-        // Construct the Discord OAuth2 authorization URL
-        const authUrl = `https://discord.com/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=identify`;
-
-        // Redirect the user to the Discord authorization URL
-        window.location.href = authUrl;
+    const getFragmentParams = () => {
+        const fragment = window.location.hash.substring(1);
+        const params: Params = {};
+        const regex = /([^&=]+)=([^&]*)/g;
+        let match;
+        while ((match = regex.exec(fragment)) !== null) {
+            params[decodeURIComponent(match[1])] = decodeURIComponent(match[2]);
+        }
+        return params;
     };
-    const handleLoginClick = async() => {
+    const handleOAuthCallback = () => {
+        const params = getFragmentParams();
+        try {
+
+            // Check if 'access_token' is present in the URL fragment
+            if ('access_token' in params) {
+                const accessToken = params['access_token'];
+                console.log()
+                window.onload = () => {
+                    const fragment = new URLSearchParams(window.location.hash.slice(1));
+                    console.log(fragment)
+                    const [accessToken, tokenType] = [params['access_token'], params['token_type']];
+                    console.log({accessToken, tokenType})
+                    fetch('https://discord.com/api/users/@me', {
+                        headers: {
+                            authorization: `${tokenType} ${accessToken}`,
+                        },
+                    })
+                        .then(result => result.json())
+                        .then(response => {
+                            const result = loginDiscord(response.username, response.email)
+                            console.log(result)
+                        })
+                        .catch(console.error);
+                };
+
+                // Now you have the access token, you can use it as needed
+                console.log('Access Token:', accessToken);
+
+                // You may want to clear the URL fragment to avoid displaying sensitive information
+                window.location.hash = '';
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+    const handleDiscordAuthClick = async () => {
+        try {
+            window.location.href = 'https://discord.com/api/oauth2/authorize?client_id=1183920234793554061&response_type=token&redirect_uri=http%3A%2F%2Flocalhost%3A5173&scope=identify+email';
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    handleOAuthCallback();
+
+    const handleLoginClick = async () => {
         try {
             if (!emailInput) {
                 setErrorMessage('Please enter a valid email address.');
@@ -51,10 +103,10 @@ const LoginRegister = () => {
                 setErrorMessage('Please enter a password.');
                 return;
             }
-            const response = await login(emailInput,passwordInput)
+            const response = await login(emailInput, passwordInput)
             setErrorMessage('');
 
-            if (response==200) {
+            if (response == 200) {
                 setErrorMessage('');
 
                 console.log('POST request successful');
@@ -68,7 +120,7 @@ const LoginRegister = () => {
 
 
     }
-    const handleRegisterClick = async() => {
+    const handleRegisterClick = async () => {
         try {
             if (!emailCreate) {
                 setErrorMessage('Please enter a valid email address.');
@@ -82,38 +134,22 @@ const LoginRegister = () => {
                 setErrorMessage('Please enter a password.');
                 return;
             }
-            const content = await register(usernameCreate, emailCreate,passwordCreate)
+            await register(usernameCreate, emailCreate, passwordCreate)
             setErrorMessage('');
-            if (!content){
-                setErrorMessage('Registration failed');
-            }
-            if (content.ok) {
-                setErrorMessage(content.status);
-                console.log('POST request successful');
-            } else {
-                console.error('POST request failed');
-            }
-            setErrorMessage('');
-
-            //request
-        setErrorMessage('');
+            setActiveTab('login');
         } catch (error) {
             console.error('Error:', error);
         }
 
     }
 
-
     return (
         <div className="dg__account section-padding--xl">
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
-                        <Tab.Container defaultActiveKey="login">
-                            <Nav
-                                variant="pills"
-                                className="acount__nav justify-content-center"
-                            >
+                        <Tab.Container activeKey={activeTab} onSelect={(key) => setActiveTab(key || 'login')}>
+                            <Nav variant="pills" className="acount__nav justify-content-center">
                                 <Nav.Item>
                                     <Nav.Link eventKey="login">Login</Nav.Link>
                                 </Nav.Item>
@@ -133,15 +169,11 @@ const LoginRegister = () => {
                                             <input type="password" value={passwordInput}
                                                    onChange={handlePasswordChange}/>
                                         </div>
-                                        <Link to={"/"}
-                                        >
-                                            Lost your password?
-                                        </Link>
                                         <button className="account__btn"
                                                 onClick={handleLoginClick}>Login
                                         </button>
                                         <button onClick={handleDiscordAuthClick} className="discord-auth-button">
-                                            <FontAwesomeIcon icon={faDiscord} className="discord-icon" />
+                                            <FontAwesomeIcon icon={faDiscord} className="discord-icon"/>
                                             Authenticate with Discord
                                         </button>
                                         {errorMessage && (
@@ -153,7 +185,7 @@ const LoginRegister = () => {
                                 <Tab.Pane eventKey="register">
                                     <div className="single__account">
                                         <div className="input__box">
-                                            <span>Usernamedd</span>
+                                            <span>Username</span>
                                             <input type="text" value={usernameCreate} onChange={handleUsernameCChange}/>
                                         </div>
                                         <div className="input__box">
