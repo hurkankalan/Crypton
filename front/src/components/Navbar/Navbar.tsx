1f0251c28e63d9459a13d25633bc117a8c6d536a
Mohammed Hikel <Mohammed-larbi.hikel@epitech.eu>
2023-12-19 15:38:52 +0100

    Added an Admin NavBar vue

--- front/src/components/Navbar/Navbar.tsx
+++ front/src/components/Navbar/Navbar.tsx
@@ -25,44 +25,63 @@ export const Navbar: React.FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.navbar}>
-        <div className={styles.navcontentLeft}>
-          <NavLink to="/home" className={styles.logo}>
-            Your Logo
-          </NavLink>
-          <div className={styles.navlinks}>
-            <NavLink to="/home" className={styles.navlink}>
-              Home
+        {role === "admin" ?
+          <>
+          <div className={styles.navcontentLeft}>
+            <NavLink to="/home" className={styles.logo}>
+              Your Logo
             </NavLink>
-            <NavLink to="/home/Markets" className={styles.navlink}>
-              Markets
-            </NavLink>
-            <NavLink to="/home/Article" className={styles.navlink}>
-              Blog
-            </NavLink>
+            <div className={styles.navlinks}>
+              <NavLink to="#" className={styles.navlink}>
+                Crypto List
+              </NavLink>
+              <NavLink to="#" className={styles.navlink}>
+                Rss Feed
+              </NavLink>
+            </div>
           </div>
-        </div>
+        </> : 
+          <>
+            <div className={styles.navcontentLeft}>
+              <NavLink to="/home" className={styles.logo}>
+                Your Logo
+              </NavLink>
+              <div className={styles.navlinks}>
+                <NavLink to="/home" className={styles.navlink}>
+                  Home
+                </NavLink>
+                <NavLink to="/home/Markets" className={styles.navlink}>
+                  Markets
+                </NavLink>
+                <NavLink to="/home/Article" className={styles.navlink}>
+                  Blog
+                </NavLink>
+              </div>
+            </div>
+          </>
+        }
         {role === "guest" ?
           <div className={styles.navcontentRight}>
             <NavLink to={""} className={styles.navlink} onClick={handleLogin}>
               Login
             </NavLink>
           </div> :
-            <>
-              <div className={styles.navcontentRight}>
-                <NavLink to="" className={styles.navlink}>
-                  Wallet
-                </NavLink>
-                <NavLink to="/home/profile" className={styles.navlink}>
-                  <div className={styles.profile}>
-                    <div className={styles.name}>{username}</div>
-                  </div>
-                </NavLink>
-                <NavLink to={""} className={styles.navlink} onClick={handleLogout}>
-                  Logout
-                </NavLink>
-              </div>
+          <>
+            <div className={styles.navcontentRight}>
+              <NavLink to="" className={styles.navlink}>
+                Wallet
+              </NavLink>
+              <NavLink to="/home/profile" className={styles.navlink}>
+                <div className={styles.profile}>
+                  <div className={styles.name}>{username}</div>
+                </div>
+              </NavLink>
+              <NavLink to={""} className={styles.navlink} onClick={handleLogout}>
+                Logout
+              </NavLink>
+            </div>
 
-            </> 
+          </>
         }
 
       </div>
