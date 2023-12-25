e49b60ec0a3ae12f5e304a34fb1d919921828afa
Mohammed Hikel <Mohammed-larbi.hikel@epitech.eu>
2023-12-25 23:59:14 +0100

    [FIX] fix some variables errors

---
+++ front/src/pages/Wallet/Wallet.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import styles from "./Wallet.module.scss";
+import { useGlobalContext } from '../../context/context';
+import { getWallet } from '../../components/api/wallet.api';
+
+interface WalletProps {
+  username: string;
+  currencyBalance: number;
+}
+
+const Wallet: React.FC<WalletProps> = () => {
+    const {username,userId,setBalance,setBitcoin,balance,bitcoin} = useGlobalContext();
+
+    useEffect(() => {
+        const Wallet = async () => {
+            try {   
+                const response = await getWallet(userId);
+                if (response) {
+                    console.log(response);
+                    setBalance(response[0].accountamount);
+                    setBitcoin(response[0].btcamount);
+                    console.log("request success", response[0].accountamount);
+                } else {
+                    console.log("Unexpected response", response);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        Wallet();
+    }, [userId]);
+    return (
+        <div className={styles.wallet}>
+            <div className={styles.wallet__title}>{username} Wallet</div>
+            <div className={styles.wallet__balance}>
+                <div className={styles.wallet__balance_item}>
+                    <div className={styles.wallet__balance_item_currency}>USD:</div>
+                    <div className={styles.wallet__balance_item_amount}>{balance}</div>
+                </div>
+                <div className={styles.wallet__balance_item}>
+                    <div className={styles.wallet__balance_item_currency}>BTC:</div>
+                    <div className={styles.wallet__balance_item_amount}>{bitcoin}</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Wallet;
