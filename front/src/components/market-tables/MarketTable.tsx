2115578ea2bdce4089b81b4e5c58b718a554adcb
Daniel Viter <daniel.viter@epitech.eu>
2024-01-06 01:00:20 +0100

    -Added connection to the backend to fetch the crypto data into the market table

--- front/src/components/market-tables/MarketTable.tsx
+++ front/src/components/market-tables/MarketTable.tsx
@@ -1,7 +1,25 @@
-import marketTableData from "./market-table-one.json";
+import {MarketTableData} from "./MarketTableSingle.js";
 import MarketTableSingle from "./MarketTableSingle.js";
 import "./MarketTable.Module.scss"
-export default function MarketTable (){
+import {GetCryptosUser} from "../api/crypto.api.tsx";
+import {useEffect, useState} from "react";
+import {useGlobalContext} from "../../context/context.ts";
+
+export default function MarketTable() {
+    const [CryptosUser, setCryptosUser] = useState([{}]);
+    const {role} = useGlobalContext();
+    useEffect(() => {
+        fetchCryptoData()
+    }, []);
+    const fetchCryptoData = async () => {
+        try {
+            const value = await GetCryptosUser();
+            setCryptosUser(value);
+            return;
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
     return (
         <div className="market__table bg--white pt--140 pb--120">
             <div className="container">
@@ -10,25 +28,24 @@ export default function MarketTable (){
                         <table className="market_table">
                             <thead>
                             <tr>
-                                <th className="market-no">
-                                    <span className="nobr">#No</span>
+                                <th className="market-symbol">
                                 </th>
-                                <th className="market-symbol">Name &amp; SYmble</th>
-                                <th className="market-prize">
-                                    <span className="nobr">Price</span>
+                                <th className="market-name">Name &amp; SYmble</th>
+                                <th className="market-current-price">
+                                    <span className="nobr">Current Price</span>
                                 </th>
-                                <th className="market-volume">
-                                    <span className="nobr">Volume</span>
+                                <th className="market-highest-price">
+                                    <span className="nobr">Highest Price</span>
                                 </th>
-                                <th className="market-dat">
-                                    <span className="nobr">%7day</span>
+                                <th className="market-lowest-price">
+                                    <span className="nobr">Lowest Price</span>
                                 </th>
                             </tr>
                             </thead>
                             <tbody>
-                            {marketTableData &&
-                                marketTableData.map((single, key) => {
-                                    return <MarketTableSingle data={single} key={key} />;
+                            {CryptosUser &&
+                                CryptosUser.map((single, key) => {
+                                    return <MarketTableSingle data={single as MarketTableData} key={key} role={role}/>;
                                 })}
                             </tbody>
                         </table>
@@ -38,4 +55,5 @@ export default function MarketTable (){
         </div>
     );
 }
+
 
