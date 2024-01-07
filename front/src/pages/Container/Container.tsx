a25de2f39d042c3c6ece34b30e0d9e1d186755bd
Mohammed Hikel <Mohammed-larbi.hikel@epitech.eu>
2024-01-07 22:26:57 +0100

    [Delete] delete le footer from the web app

--- front/src/pages/Container/Container.tsx
+++ front/src/pages/Container/Container.tsx
@@ -1,10 +1,8 @@
-import { useLocation} from "react-router-dom";
-import Footer from "../../components/Footer/Footer";
 import { Navbar } from '../../components/Navbar/Navbar';
 import styles from './Container.module.scss';
 
 export default function Container() {
-  const location = useLocation();
+
 
   return (
     <>
@@ -12,11 +10,6 @@ export default function Container() {
         <div className={styles.NavBar}>
           <Navbar />
         </div>
-        {location.pathname !== "/home/profile" ? (
-          <div className={styles.Footer}>
-            <Footer theme="white" />
-          </div>) : null
-        }
       </div>
     </>
   );
